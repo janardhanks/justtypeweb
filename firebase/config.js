907a15fc4252aefb9cef5285124052849218e240
Janardhan k s <janardhanganesh466@gmail.com>
2026-08-22 22:08:23 +0530

    Add files via upload

---
+++ firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBteHGFKdVxKTpUcVpKAYX931jR9sVKYrU",
+  authDomain: "justtypeweb-5d2aa.firebaseapp.com",
+  projectId: "justtypeweb-5d2aa",
+  storageBucket: "justtypeweb-5d2aa.firebasestorage.app",
+  messagingSenderId: "472657203695",
+  appId: "1:472657203695:web:485cea32abcb95c679a61a"
+};
